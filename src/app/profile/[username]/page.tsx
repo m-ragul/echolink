7e7dd40a6c78e53b1810a51e782a6a69fcead9f0
Ragul M <ragulm12a1@gmail.com>
2025-09-30 22:28:24 +0530

    Initial commit

---
+++ src/app/profile/[username]/page.tsx
@@ -0,0 +1,48 @@
+import {
+  getProfileByUsername,
+  getUserLikedPosts,
+  getUserPosts,
+  isFollowing,
+} from "@/actions/profile.action";
+import { notFound } from "next/navigation";
+import ProfilePageClient from "./ProfilePageClient";
+
+interface PageProps{
+  params: Promise<{
+    username: string;
+  }>;
+}
+
+export async function generateMetadata({ params }: PageProps) {
+  const {username} = await params;
+  const user = await getProfileByUsername(username);
+  if (!user) return;
+
+  return {
+    title: `${user.name ?? user.username}`,
+    description: user.bio || `Check out ${user.username}'s profile.`,
+  };
+}
+
+async function ProfilePageServer({ params }: PageProps) {
+  const {username} = await params;
+  const user = await getProfileByUsername(username);
+
+  if (!user) notFound();
+
+  const [posts, likedPosts, isCurrentUserFollowing] = await Promise.all([
+    getUserPosts(user.id),
+    getUserLikedPosts(user.id),
+    isFollowing(user.id),
+  ]);
+
+  return (
+    <ProfilePageClient
+      user={user}
+      posts={posts}
+      likedPosts={likedPosts}
+      isFollowing={isCurrentUserFollowing}
+    />
+  );
+}
+export default ProfilePageServer;
